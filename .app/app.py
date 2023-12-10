f4198884fbea4bc2749916b6f122441cee5e981a
Pedro Treviño Gongora <70229858+pedrotregon@users.noreply.github.com>
2023-12-09 20:42:00 -0600

    .

--- .app/app.py
+++ .app/app.py
@@ -1,6 +1,22 @@
-
+import pandas as pd
+import plotly.express as px
 import streamlit as st
+        
+car_data = pd.read_csv('/Users/pedrotrevino/Documents/GitHub_reposit/Sprint4/vehicles_us.csv') # leer los datos
+hist_button = st.button('Construir histograma') # crear un botón
+        
+if hist_button: # al hacer clic en el botón
+            # escribir un mensaje
+            st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+            
+            # crear un histograma
+            fig = px.histogram(car_data, x="odometer")
+        
+            # mostrar un gráfico Plotly interactivo
+            st.plotly_chart(fig, use_container_width=True)
 
-st.header('Lanzar una moneda')
+            # crear una casilla de verificación
+build_histogram = st.checkbox('Construir un histograma')
 
-st.write('Esta aplicación aún no es funcional. En construcción.')
+if build_histogram: # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
